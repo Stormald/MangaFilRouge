ea3bcddf7766fdcd8d97b6f5cda62f9871212d87
Sben65 <sbenahben@gmail.com>
2022-01-20 09:10:42 +0100

    update liste

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './components/demo/demo.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListeComponent } from './components/liste/liste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListeComponent
   ],
   imports: [
     BrowserModule,
---
+++ Front/src/app/components/liste/liste.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+declare var require: any;
+// import Anime from 'ouranilist-api';
+
+@Component({
+  selector: 'app-liste',
+  templateUrl: './liste.component.html',
+  styleUrls: ['./liste.component.css']
+})
+export class ListeComponent implements OnInit {
+
+  nom : any;
+  liste : Array<any>;
+
+  constructor() {
+    this.nom = require('ouranilist-api');
+    this.liste = [];
+  }
+
+  ngOnInit(): void {
+    this.getAnime();
+  }
+
+
+  async getAnime(){
+    
+    var anime = await this.nom.SEARCHmediasWithoutToken("One Piece", "ANIME", 1, 25);
+    this.liste = anime.data.Page.media;
+    console.log(anime);
+    console.log(this.liste);
+  }
+}
