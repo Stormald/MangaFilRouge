69af3faae923ee8d40985b5028b9b1e77146679e
Sylvain.Vasseur <sylvainvasseur2@gmail.com>
2022-01-20 09:59:51 +0100

    Affichage des données dans le component html

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListAnimeComponent } from './pages/list-anime/list-anime.component';
+import { DetailAnimeComponent } from './components/detail-anime/detail-anime.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListAnimeComponent
+    ListAnimeComponent,
+    DetailAnimeComponent
   ],
   imports: [
     BrowserModule,
