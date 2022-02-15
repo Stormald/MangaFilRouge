ce725dd9e1f91ade5904eb16e278550d64215091
ben salim <salim.benahben@cgi.com>
2022-02-15 11:05:42 +0100

    implementation du server

--- Back/Server/Repositories/UsersRepository.cs
+++ Back/Server/Repositories/UsersRepository.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using Server.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Server.Repositories
 {
-    public class UsersRepository: IUsersRepository
+    public class UsersRepository : IUsersRepository
     {
+        public void Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User FindById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
