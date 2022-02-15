2eaf2e4bd0a57706fa62577cb1dc81ae5cb38a1d
Thomas Gomes Da Silva <t.gds74@gmail.com>
2022-02-15 14:51:14 +0100

    fix scrolling after adding search

--- Front/src/app/components/liste/liste.component.ts
+++ Front/src/app/components/liste/liste.component.ts
@@ -54,11 +54,11 @@ export class ListeComponent implements OnInit {
       isAdult: false
     };
     if(searchName == null){
-      searchName = "";
+      this.searchName = "";
     }
 
     this.mediaType = "ANIME";
-    await this.nom.SEARCHmediasWithoutToken(searchName, this.mediaType, 1, 12, this.myFilter).then(data => {
+    await this.nom.SEARCHmediasWithoutToken(this.searchName, this.mediaType, 1, 12, this.myFilter).then(data => {
       this.liste = data.data.Page.media;
       this.page = data.data.Page.pageInfo.currentPage;
       this.lastPage = data.data.Page.pageInfo.lastPage;
@@ -89,6 +89,7 @@ export class ListeComponent implements OnInit {
     }
     else
       this.page++;
+      console.log("search : "+ this.searchName);
     await this.nom.SEARCHmediasWithoutToken(this.searchName, this.mediaType, this.page, 6, this.myFilter).then(data => {
       this.liste = this.liste.concat(data.data.Page.media);
       this.lastPage = data.data.Page.pageInfo.lastPage;
