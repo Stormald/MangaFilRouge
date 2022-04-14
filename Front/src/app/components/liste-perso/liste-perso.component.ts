a7d193afd6c24e3f424f367e8992492d1fd3b9e2
Thomas Gomes Da Silva <t.gds74@gmail.com>
2022-04-14 14:15:46 +0200

    Revu certaines requetes trop longues

--- Front/src/app/components/liste-perso/liste-perso.component.ts
+++ Front/src/app/components/liste-perso/liste-perso.component.ts
@@ -15,52 +15,90 @@ declare var require: any;
 export class ListePersoComponent implements OnInit {
 
   api: any;
-listePerso: Array<Category> = [{
-  catName: 'PLANNING',
-  animes: []
-}, {
-  catName: 'WATCHING',
-  animes: []
-}, {
-  catName: 'COMPLETED',
-  animes: []
-}, {
-  catName: 'PAUSED',
-  animes: []
-}, {
-  catName: 'DROPPED',
-  animes: []
-}];
-listAnimesInList : Array<ListPerso>
-user: User;
+  listePerso: Array<Category> = [{
+    catName: 'PLANNING',
+    animes: []
+  }, {
+    catName: 'WATCHING',
+    animes: []
+  }, {
+    catName: 'COMPLETED',
+    animes: []
+  }, {
+    catName: 'PAUSED',
+    animes: []
+  }, {
+    catName: 'DROPPED',
+    animes: []
+  }];
+  listAnimesInList: Array<ListPerso> = [];
+  user: User;
+  page: number = 0;
+  lastPage: number = 0;
 
-  constructor(private router: Router, private service: ListPersoService) { 
+  constructor(private router: Router, private service: ListPersoService) {
     this.api = require('ouranilist-api');
     this.router.routeReuseStrategy.shouldReuseRoute = function () {
-          return false;
+      return false;
     }
     this.user = JSON.parse(sessionStorage.getItem('currentUser'));
   }
 
   ngOnInit(): void {
-    if(this.user != null){
+    if (this.user != null) {
       this.getAnimesFromListPerso();
     }
   }
 
   async getAnimesFromListPerso() {
-    this.service.getAllListPersosByUserId(this.user.id).subscribe(data =>{
-      this.listAnimesInList = data
+    this.service.getAllListPersosByUserId(this.user.id).subscribe(async data => {
+      if (data != null) {
+        this.listAnimesInList = data.sort(x => x.animeId)
+        //console.log(data);
+      }
       //console.log(this.listAnimesInList)
+      let animeIds: number[] = [];
+
+      let animeIdsString = "[";
       this.listAnimesInList.forEach(anime => {
-        this.api.GETmediaWithLittleInfosWithoutToken(anime.animeId).then(animeInfos =>{
-          //console.log(animeInfos)
-          this.listePerso[anime.categoryListPersoId-1].animes.push({ ...anime, ...animeInfos});
-          //console.log(this.listePerso);
-        })
+        animeIds.push(anime.animeId);
+        animeIdsString = animeIdsString.concat(animeIds + ",");
       });
+      
+      //console.log(animeIdsString);
+      animeIdsString = animeIdsString.slice(0, -1);
+      animeIdsString += "]";
+
+      //console.log(animeIds)
+      //console.log(animeIdsString);
+
+      await this.getAnimesInfos(animeIdsString);
     });
-}
+  }
+
+
+  private async getAnimesInfos(animeIdsString: string) {
+    let index = 0;
+
+    do {
+      this.page += 1;
+      await this.api.GETmediaWithLittleInfosWithoutToken("ANIME", this.page, 50, animeIdsString).then(animeInfos => {
+
+        //console.log(animeInfos);
+        let liste = animeInfos.data.Page.media.sort(x => x.id);
+        this.page = animeInfos.data.Page.pageInfo.currentPage;
+        this.lastPage = animeInfos.data.Page.pageInfo.lastPage;
+        for (; index < liste.length; index++) {
+          this.listePerso[this.listAnimesInList[index].categoryListPersoId - 1].animes.push({ ...this.listAnimesInList[index], ...liste[index] });;
+        }
+        //console.log(animeInfos.data.Page.pageInfo);
+        //console.log(animeInfos.data.Page.pageInfo.lastPage);
+      });
+
+      //console.log(this.page + " : " + this.lastPage);
+    } while (this.page != this.lastPage);
+    return index;
+  }
 
   goToDetailAnime(id: number) {
     //console.log(id);
@@ -72,10 +110,10 @@ user: User;
    * @param timeStamp 
    * @returns String
    */
-   TransformTimeStamp(timeStamp): String{
-    var DateTemp = new Date(timeStamp*1000);
+  TransformTimeStamp(timeStamp): String {
+    var DateTemp = new Date(timeStamp * 1000);
     var DateTMSTP = DateTemp.toDateString() + ", " + DateTemp.toLocaleTimeString();
-   return DateTMSTP;
- }
+    return DateTMSTP;
+  }
 
 }
